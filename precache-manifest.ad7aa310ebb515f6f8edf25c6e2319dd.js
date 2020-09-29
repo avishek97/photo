26bbff6ababb98c0ba359188db724f4ce66f3cc8
avishek97 <avimishra241@gmail.com>
2020-09-29 13:41:26 +0530

    Add files via upload

---
+++ precache-manifest.ad7aa310ebb515f6f8edf25c6e2319dd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdaf3eba5cbeda493fe51ce813ca6063",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5c978830ef1658b21e9",
+    "url": "/static/css/main.4271b6b6.chunk.css"
+  },
+  {
+    "revision": "eaf53dbce892c8f70862",
+    "url": "/static/js/2.6d313c48.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6d313c48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5c978830ef1658b21e9",
+    "url": "/static/js/main.1e10c03c.chunk.js"
+  },
+  {
+    "revision": "323cb2209b48d8ff6401",
+    "url": "/static/js/runtime-main.206376fa.js"
+  },
+  {
+    "revision": "e1098f4c25b0d86546cd51182903f5ae",
+    "url": "/static/media/camera.e1098f4c.png"
+  },
+  {
+    "revision": "37f922ae9869e58830827b0e29c017eb",
+    "url": "/static/media/defaultpic.37f922ae.png"
+  }
+]);
